d43640b5557d4bdb9ef1c251f4267761fb8769b0
Sameer-Builds <sameerxdev@gmail.com>
2025-12-04 22:56:20 +0500

    created an AccordionSection

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
 import Logos from './Components/Logos'
 import MeetNylas from './Components/MeetNylas'
+import AccordionSection from './Components/AccordionSection'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Logos />
       <MeetNylas />
+      <AccordionSection />
     </div>
   )
 }
---
+++ src/Components/AccordionSection.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { useSiteData } from "../context/SiteDataContext";
+
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Fade from "@mui/material/Fade";
+
+function AccordionSection() {
+    const { AccordionSection } = useSiteData();
+    const [expanded, setExpanded] = useState(0);
+
+    const handleChange = (index) => (e, isExpanded) => {
+        if (!isExpanded) return;
+        setExpanded(index);
+    };
+
+    return (
+        <section className="container p-6 mx-auto">
+            <div className="pb-10">
+                <p className="text-center text-4xl tracking-wide font-semibold">
+                    {AccordionSection.title}
+                </p>
+            </div>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-16">
+                <div className="space-y-4">
+                    {AccordionSection.Accordion.map((item, index) => {
+                        const isOpen = expanded === index;
+                        return (
+                            <Accordion
+                                key={index}
+                                expanded={isOpen}
+                                onChange={handleChange(index)}
+                                disableGutters
+                                elevation={0}
+                                sx={{
+                                    borderBottom: "1px solid #e5e7eb",
+                                    "&:before": { display: "none" },
+                                }}  >
+                                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                                    {!isOpen && (
+                                        <Typography sx={{ fontSize: "1.25rem", fontWeight: 500 }}>
+                                            {item.title}
+                                        </Typography>
+                                    )}
+                                </AccordionSummary>
+
+                                <AccordionDetails>
+                                    <div className="bg-[#e8f0ff] rounded-2xl p-6 shadow-sm">
+
+                                        <h3 className="text-2xl font-semibold mb-4">
+                                            {item.title}
+                                        </h3>
+                                        <p className="text-gray-700 mb-6 text-lg">
+                                            {item.summary}
+                                        </p>
+                                        <button className="text-blue-600 font-medium flex items-center gap-2 text-lg group">
+                                            {item.caption}
+                                            <span className="text-xl transform transition-all duration-300 group-hover:translate-x-1">
+                                                →
+                                            </span>
+                                        </button>
+
+                                    </div>
+                                </AccordionDetails>
+                            </Accordion>
+                        );
+                    })}
+                </div>
+                <div className="flex justify-center ">
+                    <Fade key={expanded} in timeout={400}>
+                        <img
+                            src={AccordionSection.Accordion[expanded]?.image}
+                            alt="accordion visual"
+                            className="max-w-[450px] lg:max-w-[520px] w-full h-auto object-contain drop-shadow-xl"
+                        />
+                    </Fade>
+                </div>
+            </div>
+        </section>
+    );
+}
+export default AccordionSection;
